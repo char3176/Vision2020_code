0d617812b66c129a4117a141c0475aed0c4c19c2
char <charris5@gmail.com>
2020-01-30 19:10:34 -0500

    backup grip.py_proper_recogs1Contour and update jacobDev with versions from char

--- jacobDev/pi/VisionProcessingv1/G2Class.py
+++ jacobDev/pi/VisionProcessingv1/G2Class.py
@@ -1,153 +1,218 @@
-import cv2 as cv
+import cv2
 import math
 
 class G2Class:
 
-    isAG2 = False
-    
-    approxPts = []
-    centroid = []
 
-    # Quadrant Points
-    RTpts = []
-    LTpts = []
-    LBpts = []
-    RBpts = []
+    def __init__(self, cnt, approx):
+        self.isAG2 = False
 
-    #Outer Points
-    outerPts = []
-    #Inner Points
-    innerPts = []
-    #Averages
-    avgPts = []
+        self.cnts = cnt
+        self.approx = approx[0]
+        self.approxPts = []
+        self.centroid = []
+        self.cX = 0
+        self.cY = 0
 
-    #Vision Tape Constants
-    LTtoRTLen = 39.25
-    #Average between inner and outer lengths
-    SideLen = (17.5 + 19.625) / 2
-    
+        # Quadrant Points
+        self.RTpts = []
+        self.LTpts = []
+        self.LBpts = []
+        self.RBpts = []
 
-    def __init__(self, approx):
-        if len(self.approx) == 4 or 8:
-            approxPts = self.approx
+        #Outer Points
+        self.outerPts = []
+        #Inner Points
+        self.innerPts = []
+        #Averages
+        self.avgPts = []
+        self.test1 = []
+        #Vision Tape Constants
+        self.LTtoRTLen = 39.25
+        #Average between inner and outer lengths
+        self.sideLen = (17.5 + 19.625) / 2
+        #*#*#*  Need to loop over approx "len(approx)" times doing below conditional
+        numOfPointSets = len(approx)
+        #Loops over pointsets
+        print("approx:",approx)
+        for pointSetIndex in range(0,numOfPointSets):
+   	    #Checking if pointset has 4 or 8 elements
+           if len(approx[pointSetIndex]) == 4 or 8:
+                #Loops over points
+                numOfPoints = len(approx[pointSetIndex])
+                for pointsIndex in range(0, numOfPoints):
+                    coordArray = approx[pointSetIndex][pointsIndex][0]
+                    coordArray2 = coordArray.tolist()
+                    self.approxPts.append(coordArray2)
+        print("approxPts", self.approxPts)
+        red = [0,0,255]
+        tempimage = cv2.imread("test.jpg")
+        print("tempimage: ",tempimage)
+        print("tempimage len: ",len(tempimage))
+        print("tempimage[0] len: ",len(tempimage[0]))
+        approxPtsLength = len(self.approxPts)
+        print(approxPtsLength)
+        for i in range(0,approxPtsLength):
+          print("i: ", i)
+          print(self.approxPts[i][0],self.approxPts[i][1])
+          print(tempimage[self.approxPts[i][1],self.approxPts[i][0]])
+          tempimage[self.approxPts[i][1],self.approxPts[i][0]]=red
+        cv2.drawContours(tempimage, [self.approx], -1, (0, 0, 255), 2)
+        cv2.drawContours(tempimage, self.approx, -1, (0, 255, 0), 4)
+        cnt = self.cnts[0]
+        M = cv2.moments(cnt)
+        print(M)
+        self.cX = int(M["m10"] / M["m00"])
+        cX = int(M["m10"] / M["m00"])
+        self.cY = int(M["m01"] / M["m00"])
+        cY = int(M["m01"] / M["m00"]) - 20
+        print(self.cX,self.cY)
+        self.centroid = [self.cX, self.cY]
+        (x, y, w, h) = cv2.boundingRect(self.approx)
+        (startX, endX) = (int(cX - (h * 0.25)), int(cX + (h * 0.25)))
+        (startY, endY) = (int(cY - (h * 0.25)), int(cY + (h * 0.25)))
+        cv2.line(tempimage, (startX, cY), (endX, cY), (0, 255, 0), 2)
+        cv2.line(tempimage, (cX, startY), (cX, endY), (0, 255, 0), 2)
+        myradius = ((endY - startY) / 2 ) * 0.8
+        cv2.circle(tempimage, (int(cX), int(cY)), int(myradius), (0,255,0), 1)
+        cv2.circle(tempimage, (int(cX), int(cY)), int(myradius * 0.3), (0,0,255), 1)
+        cv2.imwrite('t.jpg',tempimage)
 
-    def calcCentroid():
-        #Compute the centroid of the contour
-        M = cv.Moments(approxPts)
-        cX = int(M["m10"], M["m00"])
-        cY = int(M["m01"], M["m00"])
-        centroid = [cX, cY]
 
-    def seperateIntoQuads():
-        for pnt in approxPts:
-            if pnt[0] > cX:
-                if pnt[1] < cY:
-                    RTpts.append(pnt)
+      ##### NEED TO FIX APPROXPTS ARRAY ABOVE TO INCLUDE/ACCOUNT FOR MULTIPE
+      ##### "PtsSets" incase of more than 1 contour
+      ##### ANNNDDDD decide what to do if we are fu-bar'd with more than one match later
+      ##### lower down in Jake's logic
+
+    def calcCentroid(self):
+        #Compute the centroid of the array of approxPts
+        #print(self.approxPts)
+        cnt = self.cnts[0]
+        M = cv2.moments(cnt)
+        print(M)
+        self.cX = int(M["m10"] / M["m00"])
+        self.cY = int(M["m01"] / M["m00"])
+        print(self.cX,self.cY)
+        self.centroid = [self.cX, self.cY]
+
+    def seperateIntoQuads(self):
+        for pnt in self.approxPts:
+            if pnt[0] > self.cX:
+                if pnt[1] < self.cY:
+                    self.RTpts.append(pnt)
                 else:
-                    RBpts.append(pnt)
+                    self.RBpts.append(pnt)
             else:
-                if pnt[1] < cY:
-                    LTpts.append(pnt)
+                if pnt[1] < self.cY:
+                    self.LTpts.append(pnt)
                 else:
-                    LBpts.append(pnt)
+                    self.LBpts.append(pnt)
 
-    def seperateQuadsIntoIvO():
-        if len(approxPts) == 8:
+    def seperateQuadsIntoIvO(self):
+        if len(self.approxPts) == 8:
             #Quadrant 1: RT
-            if RTpts[0][0] > RTpts[1][0]:
-                innerPts.append(RTpts[0])
-                outerPts.append(RTpts[1])
+            if self.RTpts[0][0] > self.RTpts[1][0]:
+                self.innerPts.append(self.RTpts[0])
+                self.outerPts.append(self.RTpts[1])
             else:
-                innerPts.append(RTpts[1])
-                outerPts.append(RTpts[0])
+                self.innerPts.append(self.RTpts[1])
+                self.outerPts.append(self.RTpts[0])
 
             #Quadrant 2: LT
-            if LTpts[0][0] > LTpts[1][0]:
-                innerPts.append(LTpts[0])
-                outerPts.append(LTpts[1])
+            if self.LTpts[0][0] > self.LTpts[1][0]:
+                self.innerPts.append(self.LTpts[0])
+                self.outerPts.append(self.LTpts[1])
             else:
-                innerPts.append(LTpts[1])
-                outerPts.append(LTpts[0])
+                self.innerPts.append(self.LTpts[1])
+                self.outerPts.append(self.LTpts[0])
 
             #Quadrant 3: LB
-            if LBpts[0][0] > LBpts[1][0]:
-                innerPts.append(LBpts[0])
-                outerPts.append(LBpts[1])
+            if self.LBpts[0][0] > self.LBpts[1][0]:
+                self.innerPts.append(self.LBpts[0])
+                self.outerPts.append(self.LBpts[1])
             else:
-                innerPts.append(LBpts[1])
-                outerPts.append(LBpts[0])
+                self.innerPts.append(self.LBpts[1])
+                self.outerPts.append(self.LBpts[0])
 
             #Quadrant 4: RB
-            if RBpts[0][0] > RBpts[1][0]:
-                innerPts.append(RBpts[0])
-                outerPts.append(RBpts[1])
+            if self.RBpts[0][0] > self.RBpts[1][0]:
+                self.innerPts.append(self.RBpts[0])
+                self.outerPts.append(self.RBpts[1])
             else:
-                innerPts.append(RBpts[1])
-                outerPts.append(RBpts[0])
+                self.innerPts.append(self.RBpts[1])
+                self.outerPts.append(self.RBpts[0])
+            print("RTpts: ",self.RTpts)
+            print("RBpts: ",self.RBpts)
+            print("LBpts: ",self.LBpts)
+            print("LTpts: ",self.LTpts)
 
-    def findAvgs():
-        if len(RTpts) == 2:
+    def findAvgs(self):
+        if len(self.RTpts) == 2:
             #Average the two points in each quadrant
-            XCoord = (int)(RTpts[0][0] + RTpts[1][0])/2
-            YCoord = (int)(RTpts[0][1] + RTpts[1][1])/2
-            avgPts.append([XCoord, YCoord])
-            XCoord = (int)(LTpts[0][0] + LTpts[1][0])/2
-            YCoord = (int)(LTpts[0][1] + LTpts[1][1])/2
-            avgPts.append([XCoord, YCoord])
-            XCoord = (int)(LBpts[0][0] + LBpts[1][0])/2
-            YCoord = (int)(LBpts[0][1] + LBpts[1][1])/2
-            avgPts.append([XCoord, YCoord])
-            XCoord = (int)(RBpts[0][0] + RBpts[1][0])/2
-            YCoord = (int)(RBpts[0][1] + RBpts[1][1])/2
-            avgPts.append([XCoord, YCoord])
+            XCoord = (int)(self.RTpts[0][0] + self.RTpts[1][0])/2
+            YCoord = (int)(self.RTpts[0][1] + self.RTpts[1][1])/2
+            self.avgPts.append([XCoord, YCoord])
+            XCoord = (int)(self.LTpts[0][0] + self.LTpts[1][0])/2
+            YCoord = (int)(self.LTpts[0][1] + self.LTpts[1][1])/2
+            self.avgPts.append([XCoord, YCoord])
+            XCoord = (int)(self.LBpts[0][0] + self.LBpts[1][0])/2
+            YCoord = (int)(self.LBpts[0][1] + self.LBpts[1][1])/2
+            self.avgPts.append([XCoord, YCoord])
+            XCoord = (int)(self.RBpts[0][0] + self.RBpts[1][0])/2
+            YCoord = (int)(self.RBpts[0][1] + self.RBpts[1][1])/2
+            self.avgPts.append([XCoord, YCoord])
         else:
             #If 1 point per quadrant, make the average our only point
-            avgPts.append(RTpts)
-            avgPts.append(LTpts)
-            avgPts.append(LBpts)
-            avgPts.appemd(RBpts)
+            self.avgPts.append(RTpts)
+            self.avgPts.append(LTpts)
+            self.avgPts.append(LBpts)
+            self.avgPts.appemd(RBpts)
+        print("avgRTpts: ",self.RTpts)
+        print("RBpts: ",self.RBpts)
+        print("LBpts: ",self.LBpts)
+        print("LTpts: ",self.LTpts)
 
-    def calcDistance(point1, point2):
+    def calcDistance(self,point1, point2):
         xDiff = point1[0] - point2[0]
         yDiff = point1[1] - point2[1]
         dist = math.sqrt((xDiff * xDiff) + (yDiff * yDiff))
         return dist
 
-    def amIG2():
+    def amIG2(self):
         #FIND THE FIRST TWO ANGLES
         #Find longest side using the distance formula
-        LTtoRTImgDist = calcDistance(avgPts[1], avgPts[3])
+        self.LTtoRTImgDist = self.calcDistance(self.avgPts[1], self.avgPts[3])
         #Find shortest side using the distance formula
-        xDiff = avgPts[2][0] - avgPts[3][0]
-        yDiff = avgPts[2][1] - avgPts[3][1]
-        LBtoRBImgDist = math.sqrt((xDiff * xDiff) + (yDiff * yDiff))
+        xDiff = self.avgPts[2][0] - self.avgPts[3][0]
+        yDiff = self.avgPts[2][1] - self.avgPts[3][1]
+        self.LBtoRBImgDist = math.sqrt((xDiff * xDiff) + (yDiff * yDiff))
         #Find the ratio
-        distRatioImg = LTtoRTImgDist/LBtoRBImgDist
-        distRatioReal = LTtoRTLen / sideLen
+        distRatioImg = self.LTtoRTImgDist/self.LBtoRBImgDist
+        distRatioReal = self.LTtoRTLen / self.sideLen
         #If ratios aren't similar, we're not looking at a G2
-        if abs(distRatioImg - distRatioReal) < 0.1:            
-           isAG2 = True
-           
+        if abs(distRatioImg - distRatioReal) < 0.1:
+           self.isAG2 = True
+           print("self.isAG2: ")
         #Go to next text if it passed the first
-        if isAG2:
+        if self.isAG2:
             #Eq1 refers to line equation of the line between Quads 2 and 4
             #Eq2 refers to the equation of the perp line between Quad 3
                 #and the point of intersection of eq1
             #Calculate the y=mx+b equation of eq1
-            deltaY = avgPts[3][1] - avgPts[1][1]
-            deltaX = avgPts[3][0] - avgPts[1][0]
+            deltaY = self.avgPts[3][1] - self.avgPts[1][1]
+            deltaX = self.avgPts[3][0] - self.avgPts[1][0]
             slopeEq1 = deltaY/deltaX
-            yIntEq1 = slope * avgPts[1][0]
+            yIntEq1 = slope * self.avgPts[1][0]
             #Calculate the y=mx+b equation of eq2 using point slope
             slopeEq2 = -deltaX/deltaY
-            yIntEq2 = (slopeEq2 * avgPts[2][0]) + avgPts[2][1]
+            yIntEq2 = (slopeEq2 * self.avgPts[2][0]) + self.avgPts[2][1]
             #Calculate the intersection point of Eq1 and Eq2
             xInt = (int)((yIntEq2 - yIntEq1) / (slope - slopeEq2))
             yInt = (int)(slopeEq1 * xInt) + yIntEq1
             #Find the three side lengths
-            Q2toQ3 = calcDistance(avgPts[1], avgPts[2])
-            Q3toQ4 = calcDistacne(avgPts[2], avgPts[3])
-            sharedSide = calcDistance(avgPts[2], [xInt, yInt])
+            Q2toQ3 = self.calcDistance(self.avgPts[1], self.avgPts[2])
+            Q3toQ4 = self.calcDistacne(self.avgPts[2], self.avgPts[3])
+            sharedSide = self.calcDistance(self.avgPts[2], [xInt, yInt])
             #Use trig to calculate angles
             ang1 = math.degrees(math.cos(sharedSide / Q2toQ3))
             ang2 = math.degrees(math.cos(sharedSide / Q3toQ4))
@@ -157,38 +222,37 @@ class G2Class:
             #Eq2 refers to the equation of the perp line between Quad 2
                 #and the point of intersection of eq1
             #Calculate the y=mx+b equation of eq1
-            deltaY = avgPts[2][1] - avgPts[0][1]
-            deltaX = avgPts[2][0] - avgPts[0][0]
+            deltaY = self.avgPts[2][1] - self.avgPts[0][1]
+            deltaX = self.avgPts[2][0] - self.avgPts[0][0]
             slopeEq1 = deltaY/deltaX
-            yIntEq1 = slope * avgPts[2][0]
+            yIntEq1 = slope * self.avgPts[2][0]
             #Calculate the y=mx+b equation of eq2 using point slope
             slopeEq2 = -deltaX/deltaY
-            yIntEq2 = (slopeEq2 * avgPts[3][0]) + avgPts[3][1]
+            yIntEq2 = (slopeEq2 * self.avgPts[3][0]) + self.avgPts[3][1]
             #Calculate the intersection point of Eq1 and Eq2
             xInt = (int)((yIntEq2 - yIntEq1) / (slope - slopeEq2))
             yInt = (int)(slopeEq1 * xInt) + yIntEq1
             #Find the three side lengths
-            Q4toQ1 = calcDistance(avgPts[1], avgPts[2])
-            sharedSide = calcDistance(avgPts[3], [xInt, yInt])
+            Q4toQ1 = self.calcDistance(self.avgPts[1], self.avgPts[2])
+            sharedSide = self.calcDistance(self.avgPts[3], [xInt, yInt])
             #Use trig to calculate angles
             ang3 = math.degrees(math.cos(sharedSide / Q3toQ4))
             ang4 = math.degrees(math.cos(sharedSide / Q4toQ1))
 
             #Check if the angles' sum are around 240 degrees
             if abs((ang1 + ang2 + ang3 + ang4) - 240) != 10:
-                isAG2 = False
-
-        return isAG2
+                self.isAG2 = False
 
 
-    def doEverything():
-        calcCentroid()
-        seperateIntoQuads()
-        seperateQuadsIntoIvO()
-        findAvgs()
-        amIG2()
-        print(isAG2)
 
-    doEverything()
+    def doEverything(self):
+        self.calcCentroid()
+        self.seperateIntoQuads()
+        self.seperateQuadsIntoIvO()
+        self.findAvgs()
+        self.amIG2()
+        print(self.isAG2)
 
-        
+    #doEverything()
+
+
