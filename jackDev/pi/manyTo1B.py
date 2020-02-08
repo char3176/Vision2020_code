4b84ada18ce83ed45f51cfc23966296883c606c6
Jack LePage <jackwlepage@yahoo.com>
2020-02-07 20:56:27 -0500

    Update manyTo1B.py

--- jackDev/pi/manyTo1B.py
+++ jackDev/pi/manyTo1B.py
@@ -46,7 +46,8 @@ class ManyTo1B:
             self.theOneTrueBall = maybe
 
     def calcDistance(self, radius):
-        f_x = 2719.2940207391980
+        #f_x = 2719.2940207391980 / 20
+        f_x = 77
         F_x = f_x*(.14/2*radius) #mm
         #m_F_x = F_x / 1000 #m
         return F_x
