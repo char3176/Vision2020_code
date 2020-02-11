d9a6d266319796d7a4700edf1a4d314eb9a978ed
char@odin <char@odin>
2020-02-11 13:21:06 -0500

    in process: implementing quick & dirty logging for when pi has not video output, like when it's on robot acting purely as a vision server

--- char/tictoctac.py
+++ char/tictoctac.py
@@ -1,14 +1,30 @@
 import time
+import logging
 
 class tictoctac:
 
-  def __init__(self):
+  def __init__(self, logging_verbosity = 1):
     self.instanceID = hex(id(self))
     self.instantiation_time = time.perf_counter()
     self.tic = None
     self.toc = None
     self.tac = None
     self.tac_ms = None
+
+    # IGNORE THIS FOR NOW.  CHAR IMPLEMENTING Q&D LOGGER SO TIMINGS CAN BE RECORDED
+    # TO LOG FILE FOR WHEN PI HAS NO VIDEO OUTPUT AVAILABLE
+    logging.basicConfig(filename='tictoctac.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+    if logging_verbosity == 1:
+      logging.basicConfig(level=logging.DEBUG)
+    elif logging_verbosity == 2:
+      logging.basicConfig(level=logging.INFO)
+    elif logging_verbosity == 3:
+      logging.basicConfig(level=logging.WARNING)
+    elif logging_verbosity == 4:
+      logging.basicConfig(level=logging.ERROR)
+    elif logging_verbosity == 5:
+      logging.basicConfig(level=logging.CRITICAL)
+
 
   def markTic(self):
     self.tic = time.perf_counter()
